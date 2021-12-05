052fa9ab91f1a4b017951d146e4bf55e99906765
Futaroukun <93515103+Futaroukun@users.noreply.github.com>
2021-12-05 17:36:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const os = process
 global.DeveloperMode = 'false' //true Or false
-global.owner = ['33623746892', '6289504585790'] || os.env["OWNER_NUMBER"]// Put your number here
+global.owner = ['33623746892', '6285722157719'] || os.env["OWNER_NUMBER"]// Put your number here
 global.linkGC = ['http://bit.ly/Grupgabutbotv1']
-global.mods = ['33623746892', '6289504585790'] // Want some help?
-global.prems = ['33623746892', '6289504585790'] // Premium user has unlimited limit
+global.mods = ['33623746892', '6285722157719'] // Want some help?
+global.prems = ['33623746892', '6285722157719'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -21,8 +21,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Sticker Dari Bot Adi'
-global.author = 'GH: Adi-OfficialL'
+global.packname = 'Sticker Dari Futarou-kun Botz'
+global.author = 'GH: Futarou-kun'
 
 global.multiplier = 69 // The higher, The harder levelup
 
